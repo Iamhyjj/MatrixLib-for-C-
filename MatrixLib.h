c6abc0049171648d1f3702cab2ec87c7f3e95211
Iamhyjj <2423049371@qq.com>
2024-06-27 17:44:12 +0800

    Updating

--- MatrixLib.h
+++ MatrixLib.h
@@ -3,14 +3,18 @@
 #include <memory>
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 namespace MatrixLib{
 
 template<typename T>
 class Matrix
 {
     public:
-        Matrix(size_t rows, size_t cols, std::vector<std::vector<T>> data): 
-        rows_(rows),cols_(cols),data_(std::make_unique<std::vector<std::vector<T>>>(data)){}
+        Matrix(std::vector<std::vector<T>> data): data_(std::make_unique<std::vector<std::vector<T>>>(data))
+        {
+            rows_ = data.size();
+            cols_ = data[0].size();
+        }
 
         //create a matrix whose elements are default value
         Matrix(size_t rows=0,size_t cols=0):rows_(rows),cols_(cols){
@@ -18,7 +22,20 @@ class Matrix
             data_ = std::make_unique<std::vector<std::vector<T>>>(data);
         }
 
+        //cpoy constructor
+        Matrix(const Matrix& other){
+            std::vector<std::vector<T>> dst(other.data().size(),std::vector<T>(other.data()[0].size()));
+            rows_ = other.rows();
+            cols_ = other.cols();
+            for(auto i{0};i<dst.size();i++){
+                std::copy(other.data()[i].begin(),other.data()[i].end(),dst[i].begin());
+            }
+            data_ = std::make_unique<std::vector<std::vector<T>>>(dst);
+        }
         
+        //move constructor
+        Matrix(Matrix&& other):rows_(other.rows()),cols_(other.cols()),data_(std::move(other.data_)){}
+
         size_t rows()const {return rows_;}
 
         size_t cols()const {return cols_;}
@@ -27,7 +44,7 @@ class Matrix
         
         Matrix operator+(const Matrix& other) const{
             if(rows_ != other.rows() || cols_ != other.cols()){
-                throw std::invalid_argument("Matrices dimensions mismatch");
+                throw std::invalid_argument("Matrixes dimensions mismatch");
             }
             Matrix result(rows_, cols_);
             for(size_t i = 0; i < rows_; ++i){
@@ -38,8 +55,20 @@ class Matrix
             return result;
         }
 
-        Matrix operator-(){
-            Matrix result(rows_,cols_);
+        Matrix operator+=(const Matrix& other){
+            if(rows_ != other.rows() || cols_ != other.cols()){
+                throw std::invalid_argument("Matrixes dimensions mismatch");
+            }
+            for(size_t i = 0; i < rows_; ++i){
+                for(size_t j = 0; j < cols_; ++j){
+                    (*data_)[i][j] += (*other.data_)[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Matrix operator-() const{
+            Matrix result(*this);
             for(size_t i = 0; i < rows_; ++i){
                 for(size_t j = 0; j < cols_; ++j){
                     (*result.data_)[i][j] = -(*data_)[i][j];
@@ -48,16 +77,68 @@ class Matrix
             return result;
         }
 
-        Matrix operator-(const Matrix& other){
-
+        Matrix operator-(const Matrix& other) const{
+            return *this + (-other);
         }
 
-        Matrix operator*(const Matrix& other){
-
+        Matrix operator-=(const Matrix& other){
+            return (*this) += (-other);
         }
+
+        Matrix operator*(const Matrix& other) const{
+            if(cols_ != other.rows()){
+                throw std::invalid_argument("Matrixes dimensions mismatch");
+            }
+            Matrix result(rows_,other.cols());
+            for(auto i{0}; i<rows_ ; ++i){
+			    for(auto j{0}; j<other.cols() ;++j){
+				    for(auto k{0}; k<cols_ ; ++k){
+					    (*result.data_)[i][j] += this->data()[i][k] * other.data()[k][j];
+				}
+			    }
+		    }
+            return result;
+        }
+
+        Matrix operator*=(const Matrix& other){
+            *this = (*this) * other;
+            return *this;
+        }
+
+        Matrix operator*=(double k){
+            for(auto& v:(*data_)){
+                for(auto& element: v){
+                    element *= k;
+                }
+            }
+            return *this;
+        }
+
+        Matrix operator*(double k) const{
+            Matrix result(*this);
+            return result *= k;
+        }
+
+
+        Matrix pow(unsigned int n){
+		    if(n==1){
+			    return *this;
+		    }
+		    if(n==2){
+			    return (*this) * (*this);
+		    }
+		    return n%2 ? (*this) * ((this->pow((n-1)/2)).pow(2)) : (this->pow(n/2)).pow(2);
+	    }
 
         Matrix operator=(const Matrix& other){
-
+            std::vector<std::vector<T>> dst(other.data().size(),std::vector<T>(other.data()[0].size()));
+            rows_ = other.rows();
+            cols_ = other.cols();
+            for(auto i{0};i<dst.size();i++){
+                std::copy(other.data()[i].begin(),other.data()[i].end(),dst[i].begin());
+            }
+            data_.reset(dst);
+            return *this;
         }
 
         bool operator==(const Matrix& other) const{
@@ -66,7 +147,7 @@ class Matrix
             }
             for(auto i{0}; i < rows_ ; ++i){
 			    for(auto j{0} ; j<cols_ ; ++j){
-                    if(*data_[i][j] != *(other.data()[i][j])){
+                    if((*data_)[i][j] != (other.data())[i][j]){
                         return 0;
                     }
 				}
@@ -74,12 +155,44 @@ class Matrix
             return 1;
 		}
 
-    private:
+        bool operator!=(const Matrix& other) const{
+            return !(*this == other);
+        }
+
+        Matrix tranpose(){
+            Matrix result(cols_,rows_);
+	        for(auto i{0} ; i<rows_ ; ++i){
+		        for(auto j{0} ; j<cols_ ; ++j){
+			        (*result.data_)[j][i] = (*data_)[i][j];
+		        }
+	        }
+	        return result;
+        }
+
+    protected:
         std::unique_ptr<std::vector<std::vector<T>>> data_;
         size_t rows_;
         size_t cols_;
 
 };
+
+template<typename T>
+class IdentityMatrix : public Matrix<T>
+{
+    public:
+        IdentityMatrix(size_t rows = 0) : Matrix<T>(rows,rows)
+        {
+            for(auto i{0} ; i < rows ; ++i){
+                (*Matrix<T>::data_)[i][i] = 1;
+            }
+        }
+};
+
+template<typename T>
+Matrix<T> operator*(double k, Matrix<T> m){
+    return m*k;
+}
+
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out,const Matrix<T>& m){
--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # <center> Matrix Lib for C++(Unfinished yet)
 ## Intruduction
-This is a simple Matrix Lib for C++ which is based on my former Matrix Lib for C(https://github.com/Iamhyjj/Matrix-Lib-for-C).  
+This is a simple Matrix Lib for C++ which is based on my former Matrix Lib for C(https://github.com/Iamhyjj/Matrix-Lib-for-C)( but better :) ).  
 It supports basic matrix caculations and operations:  
-(1)+,-,*  
+(1)+ (+=) , - (-=) , * (*=) , = , <<  
 (2)transpose  
-(3)determinant calculation  
-(4)Gauss-Jordan elinimation  
+(3)determinant calculation(unfinished)  
+(4)Gauss-Jordan elinimation(unfinished)  
 ## Basic features of Class Matrix
-
+(1)Template Class  
+The element type of matrix is up to you, you can declare it when creating  
+e.g. MatrixLib::Matrix<double> m();  
+(2)Using unique_ptr to store data    
+(3)Error check  
+Invalid operations will be thrown,for example , in operator+:  
+    *if(rows_ != other.rows() || cols_ != other.cols()){*  
+        *throw std::invalid_argument("Matrixes dimensions mismatch");*  
+    *}*  
